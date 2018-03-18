f408dda98c5bcd1f46ba7b6a381623b52695ccf2
BuildTools <unconfigured@null.spigotmc.org>
2018-03-18 19:35:18 +0100

    - Deleted "Spawn" Item in Teleporter

--- src/de/Dorfbewohner3000/Main/opencompass.java
+++ src/de/Dorfbewohner3000/Main/opencompass.java
@@ -3,13 +3,15 @@ package de.Dorfbewohner3000.Main;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class opencompass {
     public static Inventory com(Player p){
         Inventory inv = p.getServer().createInventory(null, 27, Main.Teleportername);
 
-        inv.setItem(4, joinevent.item("§6Spawn", "§7", Material.MAGMA_CREAM,1, (short)0 ));
+        for(int i = 1; i < 27;i++){
 
+        }
 
         return inv;
     }
